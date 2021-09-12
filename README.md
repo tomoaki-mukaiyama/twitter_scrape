55804137325bcedd921615dd6d04d67084a917c5
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-09-12 12:40:10 +0900

    add README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# twitterスクレイピング
+#  １万件のtwitterプロフィールから以下の情報を取得する  
+ ・フォロー  
+ ・フォロワー  
+ ・最新のツイートの日付(固定ツイートは除く)  
+ ・ページでdm解放か否かを検証  
+  
+#  大量のURLを捌く問題
+参照　https://nishinatoshiharu.com/rails-treat-huge-csv/
+分割してメモリ負担を減らす
+
+#  DM可否はログイン必須問題
+seleniumにログインで生成されたクッキー情報を渡してログイン状態を作る  
+
+#  スクレイピング処理（urlを受け取り、csvを生成）
+空の配列 info_array 定義  
+url100件配列
+urlを渡す  
+アクセスする  
+ページごとに取得した情報を配列にセットし,info_arrayに入れる(ネスト配列)  
+info_array << [follow, follower, latest_tweet, dm]  
+
+#  処理速度の検証
+benchmark
+
+require 'benchmark'
+
+Benchmark.bm 8 do |r|
+  r.report "実行時間:" do
+    # 計測したい処理
+  end
+end
--- run.rb
+++ run.rb
@@ -1,27 +1,82 @@
 require "selenium-webdriver"
 require "webdrivers"
-require "httparty"
 require "byebug"
-require "csv"
-
-ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+require 'benchmark'
+# require "date"
+# require "csv"
 
 url = "https://twitter.com/enginer_ni_naru"
-options = Selenium::WebDriver::Chrome::Options.new
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument("--user-agent=#{ua}")
-
-driver = Selenium::WebDriver.for(:chrome, options: options)
-driver.get(url)
-# byebug
-
-p "アクセスされた-----------------------"
-wait = Selenium::WebDriver::Wait.new(timeout: 60)
-
-wait.until { !driver.find_elements(:css, ".css-4rbku5.css-18t94o4.css-901oao.r-18jsvk2.r-1loqt21.r-1tl8opc.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0").empty? }
-follow = driver.find_elements(:css, ".css-4rbku5.css-18t94o4.css-901oao.r-18jsvk2.r-1loqt21.r-1tl8opc.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0")
-
-follow.each do |a|
-    p a.text
+@arr = []
+5.times do
+    @arr << url
 end
+
+def benchmark(set, run)
+    Benchmark.bm 5 do |r|
+        r.report "実行時間:" do
+            set
+            run
+        end
+    end
+end
+
+
+def set_options
+    ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument("--user-agent=#{ua}")
+    options
+end
+
+def scrape(options)
+    driver = Selenium::WebDriver.for(:chrome, options: options)
+    @arr.each do |url|
+        driver.get(url)
+        wait = Selenium::WebDriver::Wait.new(timeout: 60)
+
+        # ターゲット要素設定
+        follow_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]/span'
+        follower_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span'
+
+        # 読み込むまで待機
+        wait.until { !driver.find_elements(:xpath, follow_xpath).empty? }
+
+        #取得
+        follow = driver.find_element(:xpath, follow_xpath)
+        follower = driver.find_element(:xpath, follower_xpath)
+
+        # 配列に代入
+        info_array = [follow, follower]
+
+        # 出力
+        info_array.each do |a|
+            p a.text
+        end
+    end
+end
+# def scrape(options)
+#     driver = Selenium::WebDriver.for(:chrome, options: options)
+#     @arr.each do |url|
+#         driver.get(url)
+#         wait = Selenium::WebDriver::Wait.new(timeout: 60)
+#         wait.until { !driver.find_elements(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]/span').empty? }
+#         follow = driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]/span')
+#         follower = driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span')
+#         info_array = [follow, follower]
+#         info_array.each do |a|
+#             p a.text
+#         end
+#     end
+# end
+    
+benchmark(set_options, scrape(set_options))
+
+
+# info_array = []
+# File.open("#{DateTime.now.strftime("%Y%m%d%H%M%S")}.csv", "w") do |file|
+#     info_array.each do |row| 
+#        file.puts(row.to_csv) 
+#     end
+#  end
