4b1b006cbe68767619ac3448dab67b8048929232
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-09-12 22:57:27 +0900

    set urls in url.rb

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source "https://rubygems.org"
 
 gem "selenium-webdriver", "~> 3.142"
 gem "webdrivers"
-gem 'httparty'
 gem 'byebug'
 
--- run.rb
+++ run.rb
@@ -2,24 +2,10 @@ require "selenium-webdriver"
 require "webdrivers"
 require "byebug"
 require 'benchmark'
+require "./url"
+
 # require "date"
 # require "csv"
-
-url = "https://twitter.com/enginer_ni_naru"
-@arr = []
-5.times do
-    @arr << url
-end
-
-def benchmark(set, run)
-    Benchmark.bm 5 do |r|
-        r.report "実行時間:" do
-            set
-            run
-        end
-    end
-end
-
 
 def set_options
     ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
@@ -31,47 +17,45 @@ def set_options
 end
 
 def scrape(options)
-    driver = Selenium::WebDriver.for(:chrome, options: options)
-    @arr.each do |url|
+    @info_array = []
+    
+    @url_array.each do |url|
+        driver = Selenium::WebDriver.for(:chrome, options: options)
         driver.get(url)
-        wait = Selenium::WebDriver::Wait.new(timeout: 60)
-
-        # ターゲット要素設定
+        xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div[1]/div/span[1]/span'
         follow_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]/span'
         follower_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span'
-
-        # 読み込むまで待機
+        wait = Selenium::WebDriver::Wait.new(timeout: 60)
         wait.until { !driver.find_elements(:xpath, follow_xpath).empty? }
-
-        #取得
+        wait.until { !driver.find_elements(:xpath, xpath).empty? }
+        
+        
+        account_user = driver.find_element(:xpath, xpath)
         follow = driver.find_element(:xpath, follow_xpath)
         follower = driver.find_element(:xpath, follower_xpath)
-
-        # 配列に代入
-        info_array = [follow, follower]
-
-        # 出力
-        info_array.each do |a|
-            p a.text
-        end
+        
+        @info_array << [account_user, follow, follower]
+        
+        # byebug          
     end
 end
-# def scrape(options)
-#     driver = Selenium::WebDriver.for(:chrome, options: options)
-#     @arr.each do |url|
-#         driver.get(url)
-#         wait = Selenium::WebDriver::Wait.new(timeout: 60)
-#         wait.until { !driver.find_elements(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]/span').empty? }
-#         follow = driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[1]/a/span[1]/span')
-#         follower = driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span')
-#         info_array = [follow, follower]
-#         info_array.each do |a|
-#             p a.text
-#         end
-#     end
+
+scrape(set_options)
+
+@info_array.each do |array|
+    array.each do |account_info|
+        p account_info.text
+    end
+end
+
+# ターゲット要素設定
+# 読み込むまで待機
+# 取得
+# 配列に代入
+# 出力
+# info_array.each do |a|
+#     p a.text
 # end
-    
-benchmark(set_options, scrape(set_options))
 
 
 # info_array = []
@@ -80,3 +64,13 @@ benchmark(set_options, scrape(set_options))
 #        file.puts(row.to_csv) 
 #     end
 #  end
+
+# def benchmark(set, run)
+#     Benchmark.bm 5 do |r|
+#         r.report "実行時間:" do
+#             set
+#             run
+#         end
+#     end
+# end
+# benchmark(set_options, scrape(set_options))
