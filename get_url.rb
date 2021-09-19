7a9c5adf7b606a164fbdd4e005e6b2834d35617e
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-09-19 17:01:19 +0900

    commit

--- get_url.rb
+++ get_url.rb
@@ -4,7 +4,7 @@ require "byebug"
 require "csv"
 require "date"
 
-
+# seleniumのオプションをセット
 def set_options
     ua = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
     user_profile_path = "/Users/tomoakimukaiyama/Library/Application Support/Google/Chrome/Default"
@@ -19,10 +19,13 @@ def set_options
     capabilities #return
 end
 
+
 def get_url(options)
+
   driver = Selenium::WebDriver.for(:chrome, desired_capabilities: options)
   url_array = []
-
+  
+  # studioのプロフィール URLを配列取得
   while true do
     page_num = 1
     sleep 1
@@ -45,30 +48,23 @@ def get_url(options)
     page_num ++
   end
 
+  #プロフィールにtwitterURLがあれば、配列に入れていく
   @twitter_urls = []
   url_array.each do |url|
     sleep 1
     driver.get(url)
     sns_bar = driver.find_element(:xpath, '//*[@id="__layout"]/div/div[4]/div[2]/main/div[3]/section[1]/div[3]/div[1]/div[1]')
-    sns_icons = sns_bar.find_elements(:tag_name,'a')
-    if sns_icons > 0
       sns_bar.find_elements(:tag_name,'a').each do |icon|
         if icon.attribute("href").include?("twitter")
           @twitter_urls << icon.attribute("href")
-        each_with_index 
-      end 
-    end
-    # rescue Selenium::WebDriver::Error::NoSuchElementError
-    # p 'No Such Element Error'
+        end 
+      end
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+    p 'No Such Element Error'
   end
 end
   
-get_url(set_options)
 
-# # ./csv_files/twit_data-(日時).csv で新規保存
-CSV.open("./csv_files/twitter-urls-#{DateTime.now.strftime("%Y_%m_%d-%H:%M")}.csv", "w") do |csv|
-    p "csv書き出し中"
-    @twitter_urls.each do |array|
-        csv << array
-    end
-end 
+
+
+get_url(set_options)
